75b42d0d72f914f5c993569e197651295f3a8934
Kirill Pudov <kereeell@Kirills-MacBook-Pro.local>
2024-12-18 12:59:23 +0300

    add docker-compose and build all aps

---
+++ backend/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python3 /app/manage.py migrate
+
+python3 /app/manage.py runserver 0:8000
--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -57,11 +57,14 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('BACK_DB_NAME'),
+        'USER': os.getenv('BACK_DB_USER'),
+        'PASSWORD': os.getenv('BACK_USER_PASSWORD'),
+        'HOST': os.getenv('BACK_DB_HOST'),
+        'PORT': os.getenv('BACK_DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
